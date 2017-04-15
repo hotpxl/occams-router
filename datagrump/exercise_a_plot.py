7ce25bc341a02bd0982b097e5662d4843e85b91a
Yutian Li <hotpxless@gmail.com>
2017-04-15 12:55:21 -0700

    Plot different sizes.

--- datagrump/exercise_a_plot.py
+++ datagrump/exercise_a_plot.py
@@ -11,19 +11,29 @@ def main():
     if len(sys.argv) != 2:
         print('Usage: {} <log file>'.format(sys.argv[0]))
         sys.exit(1)
-    throughputs = []
-    delays = []
+    throughputs = {}
+    delays = {}
     with open(sys.argv[1]) as f:
         for l in f:
+            if 'Window size' in l:
+                current_size = int(l.split()[2])
+                throughputs.setdefault(current_size, [])
+                delays.setdefault(current_size, [])
             if 'throughput' in l:
-                throughputs.append(float(l.split()[2]))
+                throughputs[current_size].append(float(l.split()[2]))
             elif 'signal delay' in l:
-                delays.append(float(l.split()[4]))
-    plt.scatter(throughputs, delays)
+                delays[current_size].append(float(l.split()[4]))
+    for window_size in throughputs:
+        plt.scatter(
+            throughputs[window_size],
+            delays[window_size],
+            marker='x',
+            label=window_size)
     plt.gca().invert_xaxis()
     plt.ylabel('Throughput (Mbits/s)')
     plt.xlabel('Delay (ms)')
     plt.title('Fixed window sizes')
+    plt.legend()
     plt.savefig('exercise_a.png', bbox_inches='tight')
 
 
