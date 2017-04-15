023e5b559cf6f1af7d048cb82ef6e12e57eaf8bd
Yutian Li <hotpxless@gmail.com>
2017-04-14 22:26:35 -0700

    Exercise A script and output.

---
+++ datagrump/exercise_a.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+for ((WINDOW_SIZE = 2; WINDOW_SIZE <= 512; WINDOW_SIZE *= 2)); do
+  cp -f controller-a.cc controller.cc
+  sed -i -e "s/WINDOW_SIZE_CHANGE_ME/${WINDOW_SIZE}/g" controller.cc
+  make > /dev/null
+  echo "Window size: ${WINDOW_SIZE}"
+  ./run-contest yy 2>&1
+  ./run-contest yy 2>&1
+  ./run-contest yy 2>&1
+done
